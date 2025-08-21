fadabdae4275b80321e2aa3cfc423ac571501093
aadi106 <148841816+aadi106@users.noreply.github.com>
2025-08-21 11:28:24 +0530

    first commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var ctrllocation = require('../controllers/location');
-var ctrlothers = require('../controllers/others');
+var ctrlothers = require('../controllers/other');
 
 router.get('/',ctrllocation.homepage);
 router.get('/location',ctrllocation.location);
 router.get('/location/review',ctrllocation.reviewpage);
-router.get('/location/about',ctrlothers.about);
+router.get('/location/about',ctrlother.about);
 
 module.exports = router;
